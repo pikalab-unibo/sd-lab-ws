fb1581b68fe447bdd8c593fd00d592fe48320cd9
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2021-11-10 18:08:02 +0100

    remove content which only made sense in ay2021

--- code/CustomerController.java
+++ code/CustomerController.java
@@ -1,12 +1,16 @@
 interface CustomerController {
   // GET /customers
-  void getAll(Context ctx) throws Exception;
+  void getAll(Context ctx) throws HttpResponseException;
+  
   // GET /customers/:identifier
-  void get(Context ctx) throws Exception;    
+  void get(Context ctx) throws HttpResponseException;  
+    
   // POST /customers/
-  void post(Context ctx) throws Exception;   
+  void post(Context ctx) throws HttpResponseException;   
+  
   // PUT /customers/:identifier
-  void put(Context ctx) throws Exception;    
+  void put(Context ctx) throws HttpResponseException;   
+   
   // DELETE /customers/:identifier
-  void delete(Context ctx) throws Exception; 
+  void delete(Context ctx) throws HttpResponseException; 
 }
